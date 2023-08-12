e1916bc00109e004925e185928e0e7b9e7195804
Ayodelefasulu <fasuluayodele@gmail.com>
2023-08-11 20:44:31 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int single;
+
+	for (single = 0; single < 10; single++)
+	{
+		putchar(single);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
