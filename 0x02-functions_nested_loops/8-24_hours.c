3ad214e1c02aa8cecbca02725569089de111b578
Ayodelefasulu <fasuluayodele@gmail.com>
2023-08-15 21:16:16 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,14 @@
 #include "main.h"
+
+
 /**
- * jack_bauer - prints every minu * te of the day of Jack Bauer
- * starting from 00:00 to 23:59
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * Return: void
  */
 void jack_bauer(void)
 {
 	int i, j;
+
 	i = 0;
 
 	while (i < 24)
@@ -20,7 +23,7 @@ void jack_bauer(void)
 			_putchar((j % 10) + '0');
 			_putchar('\n');
 			j++;
-			}
-			i++;
 		}
+		i++;
 	}
+}
