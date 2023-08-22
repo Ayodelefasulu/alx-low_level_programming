c85cb56bd4cf6253d1ecc3424715c1eab6a74706
Ayodelefasulu <fasuluayodele@gmail.com>
2023-08-21 20:11:58 -0700

    task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: Pointer to the string.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+
+	/* Handle the sign */
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (s[i] == '+')
+	{
+		i++;
+	}
+
+	/* Convert string to integer */
+	while (s[i] != '\0')
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+		}
+		else
+		{
+			break; /* Stop at non-digit character */
+		}
+
+		i++;
+	}
+	return (result * sign);
+}
