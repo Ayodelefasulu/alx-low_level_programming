63c0f29a3e89aecfbe14afc6222f7e70f1525015
Ayodelefasulu <fasuluayodele@gmail.com>
2023-10-06 00:38:26 -0700

    task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+/**
+ * append_text_to_file - appends to file
+ * @filename: name of file
+ * @text_content: content of file
+ *
+ * Return: returns success or failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t fw;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		fw = write(fd, text_content, strlen(text_content));
+		if (fw == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
