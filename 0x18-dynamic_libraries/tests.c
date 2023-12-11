dfbf11c97d4d8cfae1bbb7dabfb43e2b0d1a63d8
Ayodelefasulu <fasuluayodele@gmail.com>
2023-12-11 08:11:04 -0800

    task 100

---
+++ 0x18-dynamic_libraries/tests.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - this function adds
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: returns an integer
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - this function subs
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: returns an integer
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - this function muls
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: returns an integer
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - this function divs
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: returns an integer
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - this function mods
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: returns an integer
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
