624cf3631401e845b8d0c6bdddef927c88e2110f
Ayodelefasulu <fasuluayodele@gmail.com>
2023-12-11 07:41:00 -0800

    task 0 corrected

--- 0x18-dynamic_libraries/_atoi.c
+++ 0x18-dynamic_libraries/_atoi.c
@@ -1,2 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+
+/**
+ * _atoi - This function converts ASCII to integer
+ * @s: This is the string to be converted
+ *
+ * Return: Returns and integer
+ */
+int _atoi(char *s)
+{
+	/* This function does nothing */
+}
