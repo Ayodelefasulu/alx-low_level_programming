2021b938473785fef2b97359aef0d14efa08eb00
Ayodelefasulu <fasuluayodele@gmail.com>
2023-12-22 17:47:54 -0800

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - hash table to be created
+ * @size: size of the hash table
+ *
+ * Return: Return a memory address of the hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
