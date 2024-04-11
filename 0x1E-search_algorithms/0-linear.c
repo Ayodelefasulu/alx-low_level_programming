75f4dec29d90ed3ef8f087b719325045a1e3a66e
Ayodelefasulu <fasuluayodele@gmail.com>
2024-04-10 21:28:15 -0700

    task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,16 @@
+#include "search_algos.h"
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld]\n", i);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
+
